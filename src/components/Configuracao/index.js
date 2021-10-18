b658141c6ad709d807816161cd22b3161da1e7bd
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-10-18 18:34:50 -0300

    Update index.js

--- src/components/Configuracao/index.js
+++ src/components/Configuracao/index.js
@@ -1,18 +1,19 @@
 import Link from 'next/link'
-import { Container, Box, ConfigIcon, DataIcon, FeedIcon, Helpcon, LanguageIcon, LocalIcon,  MoonIcon, BoardIcon } from './styles';
+import { Container, Box, ConfigIcon, DataIcon, FeedIcon, Helpcon, LanguageIcon, LocalIcon, MoonIcon, BoardIcon } from './styles';
 
-function Configuracao() {
+function Configuracao({ togleTheme }) {
+
     return (
         <Container>
             <Box>
-                <Link href='/teste'><a><MoonIcon/> Aparência: Tema do Dispositivo</a></Link>
-                <Link href='/teste'><a><LanguageIcon/> idioma: português</a></Link>
-                <Link href='/teste'><a><LocalIcon/> local: Brasil</a></Link>
-                <Link href='/teste'><a><ConfigIcon/> configurações</a></Link>
-                <Link href='/teste'><a><DataIcon/> seus dados no youtube</a></Link>
-                <Link href='/teste'><a><Helpcon/> ajuda</a></Link>
-                <Link href='/teste'><a><FeedIcon/> enviar feedback</a></Link>
-                <Link href='/teste'><a><BoardIcon/> atalhos do teclado</a></Link>
+                <button onClick={togleTheme}><MoonIcon /> Aparência: Tema do Dispositivo</button>
+                <Link href='/teste'><a><LanguageIcon /> idioma: português</a></Link>
+                <Link href='/teste'><a><LocalIcon /> local: Brasil</a></Link>
+                <Link href='/teste'><a><ConfigIcon /> configurações</a></Link>
+                <Link href='/teste'><a><DataIcon /> seus dados no youtube</a></Link>
+                <Link href='/teste'><a><Helpcon /> ajuda</a></Link>
+                <Link href='/teste'><a><FeedIcon /> enviar feedback</a></Link>
+                <Link href='/teste'><a><BoardIcon /> atalhos do teclado</a></Link>
             </Box>
             <Box>
                 <Link href='/teste'><a>modo restrito: desativado</a></Link>
